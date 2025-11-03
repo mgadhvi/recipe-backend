653471be6d7d9b5a1b585e80de01111e48a278a0
Manish Gadhvi <Manish.Gadhvi@lafosse.com>
2025-11-03 15:56:58 +0000

    changes to index.js

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ app.get('/recipes', (req, res) => {
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`)
 })
+
+app.get("/recipes/:name", (req, res) => {
+    const name = req.params.name.toLowerCase();
+    const recipe = recipes.find((recipe) => recipe.name.toLowerCase() == name);
+    if (recipe == undefined) {
+        res.status(404).send("The recipe doesn't exist.");
+    } else {
+        res.status(200).send(recipe);
+    }
+});
