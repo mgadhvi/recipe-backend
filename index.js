315a5fce84971411fc9a1f95fe37ea79d6fc3d68
Manish Gadhvi <Manish.Gadhvi@lafosse.com>
2025-11-04 13:15:09 +0000

    Added CORS

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const recipes = require('./models/recipes.json')
 const app = express()
+const cors = require('cors')
 const port = 3000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
    res.send('Recipes API')
